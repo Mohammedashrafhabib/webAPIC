55997e8e27d5ae153e0d92b789d0047fa82b6c34
Mohammedashrafhabib <93453956+Mohammedashrafhabib@users.noreply.github.com>
2022-07-31 15:07:25 +0200

    done +a few things in task

---
+++ webAPI/webAPI.Mvc/Controllers/productController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using webAPI.Application.Interfaces;
+using webAPI.Application.ViewModels;
+
+namespace webAPI.Mvc.Controllers
+{
+    //[Authorize]
+    public class productController : Controller
+    {
+        private IproductService _productService;
+        public productController(IproductService iproductService)
+        {
+            _productService = iproductService;
+        }
+        public IActionResult Index()
+        {
+            productViewModel model=_productService.GetProducts();
+
+            return View(model);
+        }
+    }
+}
