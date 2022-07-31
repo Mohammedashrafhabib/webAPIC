424339260e70f50cc3f75a523e5aaad4f2538a81
Mohammedashrafhabib <93453956+Mohammedashrafhabib@users.noreply.github.com>
2022-07-31 13:29:29 +0200

    comit product sevice and repositry interfaces

---
+++ webAPI/webAPI.Application/Interfaces/IproductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using webAPI.Application.ViewModels;
+using webAPI.Domain.Models;
+
+namespace webAPI.Application.Interfaces
+{
+    public interface IproductService
+    {
+        IEnumerable<productViewModel> GetProducts();
+    }
+}
---
+++ webAPI/webAPI.Domain/Interfaces/IproductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using webAPI.Domain.Models;
+
+namespace webAPI.Domain.Interfaces
+{
+    public interface IproductRepository
+    {
+        IEnumerable<product> GetProducts();
+    }
+}
