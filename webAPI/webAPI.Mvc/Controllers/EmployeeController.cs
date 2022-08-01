210f90ece634d741a1332194d32217addabf8d42
Mohammedashrafhabib <93453956+Mohammedashrafhabib@users.noreply.github.com>
2022-08-01 14:26:36 +0200

    final

--- webAPI/webAPI.Mvc/Controllers/EmployeeController.cs
+++ webAPI/webAPI.Mvc/Controllers/EmployeeController.cs
@@ -19,38 +19,39 @@ namespace webAPI.Mvc.Controllers
 
 
 
-        //[HttpGet]
-        //[Route("/getemployee")]
-        //public IActionResult Index()
-        //{
-
-        //    EmployeeViewModel model = _employeeService.GetEmployee();
-        //    EmployeeViewModel model1 = _employeeService.GetEmployee();
-        //    EmployeeViewModel model2 = _employeeService.GetEmployee();
-        //    model.employees = from e in model1.employees
-        //                      join e1 in model2.employees on e.ReprotsTo equals e1.Id into x
-        //                      from data_B in x.DefaultIfEmpty(new Employee { ReprotsTo=0})
-        //                      select new Employee
-        //                      {
-        //                          Id = e.Id,
-        //                          Name = e.Name,
-        //                          ReprotsTo = data_B.Id
-
-        //                      };
-
-        //    model.employees = from e in model.employees
-        //                      where (e.Id!=2 && e.ReprotsTo!=0)||e.Id==2
-        //                      select e;
-        //    return View(model);
-        //}
-        [HttpPost]
-        [Route("/addemployee")]
-        public IActionResult Index(int reptts, string name)
+        [HttpGet]
+        [Route("/getemployee")]
+        public IActionResult Index()
         {
-            Employee employee = new Employee { ReprotsTo = reptts, Name = name };
-            _employeeService.addEmployee(employee);
-            return View();
+
+            EmployeeViewModel model = _employeeService.GetEmployee();
+            EmployeeViewModel model1 = _employeeService.GetEmployee();
+            EmployeeViewModel model2 = _employeeService.GetEmployee();
+            model.employees = (from e in model1.employees
+                               join e1 in model2.employees on e.ReprotsTo equals e1.Id into x
+                               from data_B in x.DefaultIfEmpty(new Employee { ReprotsTo = 0 })
+                               select new Employee
+                               {
+                                   Id = e.Id,
+                                   Name = e.Name,
+                                   ReprotsTo = data_B.ReprotsTo
+
+                               }).Concat(model1.employees)
+                             ;
+
+            model.employees = from e in model.employees
+                              where (e.Id != 2 && e.ReprotsTo != 0) || e.Id == 2
+                              select e;
+            return View(model);
         }
+        //[HttpPost]
+        //[Route("/addemployee")]
+        //public IActionResult Index(int reptts, string name)
+        //{
+        //    Employee employee = new Employee { ReprotsTo = reptts, Name = name };
+        //    _employeeService.addEmployee(employee);
+        //    return View();
+        //}
     }
 }
 
